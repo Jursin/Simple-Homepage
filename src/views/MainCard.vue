a0bb24b39b0221caa9d9e4fcf6ff9d7d733de513
Jursin <jursin@126.com>
2025-11-15 02:00:06 +0800

    修改页脚信息

--- src/views/MainCard.vue
+++ src/views/MainCard.vue
@@ -162,8 +162,7 @@
 
     <div class="footer">
       <p>
-        ©2025 Quenan |
-        <a href="https://github.com/QNquenan/Simple-Homepage">仓库</a>
+        <a href="http://beian.miit.gov.cn/" target="_blank" >鄂ICP备2025157296号-1</a> | 版权所有 © 2025 Jursin
       </p>
     </div>
   </div>
